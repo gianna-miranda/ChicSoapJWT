c2410c43a16b83b9b3fa794e0d82f03851a65c7f
Gianna <giannar.miranda@gmail.com>
2019-11-22 13:31:04 -0500

    Fixed server.js file causing issues with heroku

--- server.js
+++ server.js
@@ -1,35 +1,43 @@
-require("dotenv").config();
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
+require('dotenv').config()
+
+const express = require('express')
+const logger = require('morgan')
+const mongoose = require('mongoose')
+const usersRoutes = require('./routes')
 const productsAPI = require("./routes/productsAPI");
-const usersRoutes = require("./routes");
-const app = express();
-const mongouri = process.env.ATLAS_URI;
-const port = process.env.PORT;
+const app = express()
+
+const PORT = 3001
+
+const MONGODB_URI = process.env.ATLAS_URI;
+const PORT = process.env.PORT;
 
 mongoose.set("useCreateIndex", true);
-mongoose.connect(mongouri, { useNewUrlParser: true }, err => {
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, err => {
   console.log(err || `Connected to MongoDB.`);
 });
+  const connection = mongoose.connection;
+  connection.once('open', () => {
+	console.log("MongoDB connection is live");
+  })
 
-app.use(express.static(`${__dirname}/client/build`));
-app.use(logger("dev"));
-app.use(express.json());
+app.use(express.static(`${__dirname}/client/build`))
+app.use(logger('dev'))
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/api", (req, res) => {
-  res.json({ message: "API root" });
-});
-
-app.use("/api", productsAPI);
-
-app.use("/api/users", usersRoutes);
-
-app.use("*", (req, res) => {
-  res.sendFile(`${__dirname}/client/build/index.html`);
-});
-
-app.listen(port, err => {
-  console.log(err || `Server running on port ${port}.`);
-});
+	res.json({ message: "API root" });
+  });
+  
+  app.use("/api", productsAPI);
+  app.use('/users/?*', function(){});
+  app.use("/api/users", usersRoutes);
+  
+  app.use("*", (req, res) => {
+	res.sendFile(`${__dirname}/client/build/index.html`);
+  });
+  
+  app.listen(PORT, err => {
+	console.log(err || `Server running on port ${PORT}.`);
+  });
