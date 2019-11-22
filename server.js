0a06eb371ddb38ce78044022925aa15bc31fff2e
Gianna <giannar.miranda@gmail.com>
2019-11-22 13:23:58 -0500

    Fixed server.js file causing issues with heroku

--- server.js
+++ server.js
@@ -1,50 +1,35 @@
-require('dotenv').config()
-
-const express = require('express')
-const logger = require('morgan')
-const mongoose = require('mongoose')
-const usersRoutes = require('./routes')
+require("dotenv").config();
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 const productsAPI = require("./routes/productsAPI");
-const app = express()
+const usersRoutes = require("./routes");
+const app = express();
+const mongouri = process.env.ATLAS_URI;
+const port = process.env.PORT;
 
-const PORT = 3001
+mongoose.set("useCreateIndex", true);
+mongoose.connect(mongouri, { useNewUrlParser: true }, err => {
+  console.log(err || `Connected to MongoDB.`);
+});
 
-mongoose.set('useCreateIndex', true)
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
-	uri = process.env.ATLAS_URI  // connection string for Atlas here  
-  } else {
-	uri = process.env.LOCAL_URI   // connection string for localhost mongo here  
-  }
-  
-  // connection to database
-  mongoose.connect(process.env.ATLAS_URI, { 
-	useNewUrlParser: true,
-	useFindAndModify: false,
-	useCreateIndex: true 
-  });
-  const connection = mongoose.connection;
-  connection.once('open', () => {
-	console.log("MongoDB connection is live");
-  })
-
-app.use(express.static(`${__dirname}/client/build`))
-app.use(logger('dev'))
-app.use(express.json())
+app.use(express.static(`${__dirname}/client/build`));
+app.use(logger("dev"));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/api', (req, res) => {
-	res.json({message: "API root"})
-})
+app.get("/api", (req, res) => {
+  res.json({ message: "API root" });
+});
 
 app.use("/api", productsAPI);
-app.use('/api/users', usersRoutes)
-app.use('/users/?*', function(){});
 
-app.use('*', (req, res) => {
-	res.sendFile(`${__dirname}/client/build/index.html`)
-})
+app.use("/api/users", usersRoutes);
 
-app.listen(PORT, (err) => {
-	console.log(err || `Server running on port ${PORT}.`)
-})
+app.use("*", (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`);
+});
+
+app.listen(port, err => {
+  console.log(err || `Server running on port ${port}.`);
+});
